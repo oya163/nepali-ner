7c7cb016ceded898b8cd6f3b8c4dd13877fbc683
oya163 <oyeshsin@hotmail.com>
2020-08-14 18:43:12 -0400

    Reverted and fixed

--- app.py
+++ app.py
@@ -3,26 +3,41 @@
     Date - 08/14/2020
 """
 
+import torch
+import logging
+import sys
 from flask import Flask, render_template, request
 from utils.dataloader2 import Dataloader
-import utils.utilities as utilities
-from models.models import LSTMInferer
+from models.models import LSTMTagger
 from config.config import Configuration
-import torch
 
 app = Flask(__name__)
 
 
+def get_logger():
+    logger = logging.getLogger("logger")
+    logger.setLevel(logging.DEBUG)
+    logging.basicConfig(format="%(message)s", level=logging.INFO)
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setFormatter(logging.Formatter(
+        "%(levelname)s:%(message)s"
+    ))
+    logging.getLogger().addHandler(handler)
+    return logger
+
+
 def get_config():
     config_file = "./config/config.ini"
-    log_file = './logs/complete.log'
-    logger = utilities.get_logger(log_file)
+    logger = get_logger()
     config = Configuration(config_file=config_file, logger=logger)
     config.model_file = './saved_models/lstm_1.pth'
+    config.vocab_file = './vocab/vocab.pkl'
+    config.label_file = './vocab/labels.pkl'
     config.device = 'cpu'
     config.verbose = False
     config.eval = False
     config.use_pos = False
+    config.infer = True
     return config
 
 
@@ -31,36 +46,25 @@ def pred_to_tag(dataloader, predictions):
 
 
 def infer(config, dataloader, model):
-    """
-    Prints the result
-    """
-    # Tokenize the sentence and aspect terms
-    # print(sent.split())
     sent_tok = config.txt
-
-    # Get index from vocab
     X = [dataloader.txt_field.vocab.stoi[t] for t in sent_tok]
-
-    # Convert into torch and reshape into [batch, sent_length]
     X = torch.LongTensor(X).to(config.device)
     X = X.unsqueeze(0)
 
-    # Get predictions
     pred = model(X, None)
-
     pred_idx = torch.max(pred, 1)[1]
 
     y_pred_val = pred_idx.cpu().data.numpy().tolist()
     pred_tag = pred_to_tag(dataloader, y_pred_val)
+
     return pred_tag
 
 
 # Inference section
 def inference(config):
     dataloader = Dataloader(config, '1')
-    model = LSTMInferer(config).to(config.device)
+    model = LSTMTagger(config, dataloader).to(config.device)
     model.load_state_dict(torch.load(config.model_file)['state_dict'])
-
     pred_tag = infer(config, dataloader, model)
     return pred_tag
 
